fe4d55918853c6652ea599bf522a2536061df0ff
Zonayed Ahmed <zonayedpca@yahoo.com>
2017-11-19 15:06:53 +0530

    contents updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
 (function($) {
 'use strict';
+    var navBar = document.querySelector('.navbar');
+    var navBarOffset = navBar.offsetTop;
+    
+    document.addEventListener('scroll', function() {
+        if(window.scrollY >= navBarOffset) {
+            navBar.classList.add('fixed-top');
+        } else {
+            navBar.classList.remove('fixed-top');
+        }
+    });
     
     //Cycle Text
     var cycleTextSel = $('.intro > h1');
     
     cycleTextSel.cycleText({
         animation: 'bounceIn',
-        interval: 300000
+        interval: 3000
     });
 
 })(jQuery);
