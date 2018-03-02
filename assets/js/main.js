b003974338792e7c62d0f417c1d750d76ed0af77
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-03-02 18:04:41 +0530

    zonayedpca all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 (function($) {
 'use strict';
-    var navBar = document.querySelector('.navbar');
+    var navBar = document.querySelector('.navbar-light');
     var navBarOffset = navBar.offsetTop;
+    var body = document.querySelector('body');
     
     document.addEventListener('scroll', function() {
         if(window.scrollY >= navBarOffset) {
             navBar.classList.add('fixed-top');
+            body.classList.add('mtop');
         } else {
             navBar.classList.remove('fixed-top');
+            body.classList.remove('mtop');
         }
     });
     
@@ -17,6 +20,10 @@
     cycleTextSel.cycleText({
         animation: 'bounceIn',
         interval: 3000
+    });
+    
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        offset: 100
     });
 
 })(jQuery);
